dcdc5499d7597da7de1a4d5820057173d2e643dd
jcarloslm <juancarlos0728@gmail.com>
2023-09-12 10:29:04 -0700

    "Actualizacion de Variables de entorno"

--- Server/variableEntorno.js
+++ Server/variableEntorno.js
@@ -1,7 +1,7 @@
 process.env.PORT = process.env.PORT || 3045;
-process.env.IP = process.env.IP || "192.168.9.122"
-process.env.USER = process.env.USER || "root"
-process.env.DB = process.env.DB || "cie"
-process.env.DB = process.env.PASSWORD || 'a1d2m3i4n5'
+process.env.IP = process.env.IP || '192.168.9.122'
+process.env.USER = process.env.USER || 'root'
+process.env.DB = process.env.DB || 'cie'
+process.env.PASSWORD = process.env.PASSWORD || 'a1d2m3i4n5'
 
 
